67fd76a47dac909879cdb9a368a218ed45e6c98e
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2023-11-23 13:42:12 -0300

    fix retangulo face

--- face_recognition/main.py
+++ face_recognition/main.py
@@ -2,12 +2,14 @@ import cv2
 import face_recognition
 
 # Aqui carregamos a imagem que queremos comparar
-img = cv2.imread("./images/Djalma2.png")
+img = cv2.imread("./images/Lucas.jpg")
+img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
 # Aqui carregamos a 2 imagem que queremos comparar
-img2 = cv2.imread("./images/Djalma.jpg")
+img2 = cv2.imread("./images/Lucas2.jpeg")
+img2 = cv2.resize(img2, (0, 0), fx=0.25, fy=0.25)
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
 
---
+++ face_recognition/webcan_main.py
@@ -0,0 +1,32 @@
+import cv2
+from simple_facerec import SimpleFacerec
+
+# codificar os rosto da pasta
+sfr = SimpleFacerec()
+sfr.load_encoding_images("../images/faces")
+
+# carregar camera
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+    
+    # Detectar rostos
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        # cordenadas do rosto
+        top, left, bottom, right = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        
+        cv2.putText(frame, name,(left, top - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    # tecla S para o loop
+    if key == ord('p'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()    
