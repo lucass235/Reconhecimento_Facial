75cd40fc53cfe097d413b7af6373311c8bf429b9
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2023-11-23 15:12:04 -0300

    minor

--- face_recognition/webcan_main.py
+++ face_recognition/webcan_main.py
@@ -18,12 +18,20 @@ while True:
         # cordenadas do rosto
         top, left, bottom, right = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
         
+        label = "Acesso Liberado"
+        font = cv2.FONT_HERSHEY_DUPLEX
+        font_scale = 1
+        font_thickness = 2
+        text_size, _ = cv2.getTextSize(label, font, font_scale, font_thickness)
+        bottom_left = (int((frame.shape[1] - text_size[0]) / 2), frame.shape[0] - 30)
+        
         if (name == "Lucas"):
-            cv2.putText(frame, "Bem vindo Lucas!",(left - 220, top - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 2)
+            label = label + " " + name
+            cv2.putText(frame, label, bottom_left, font, font_scale, (0, 255, 0), font_thickness)
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         else:
-            label = "Desconhecido Acesso Negado!" if name == "Unknown" else name
-            cv2.putText(frame, label,(left - 220, top - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+            label = "Acesso Negado" + " " + (name if name != "Unknown" else "Desconhecido")
+            cv2.putText(frame, label, bottom_left, font, font_scale, (0, 0, 255), font_thickness)
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
             
 
